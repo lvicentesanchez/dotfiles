fe87c0eaa7852b06748b241f2cf45e2517af3b26
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2014-12-09 02:40:54 +0000

    * replace powerline with smart-mode-line.

--- .emacs.d/config/01_theme.el
+++ .emacs.d/config/01_theme.el
@@ -1,3 +1,5 @@
+(setq inhibit-splash-screen t)
+
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,12 +1,15 @@
 (defvar emacs-dir (file-name-directory load-file-name)
   "The root dir of the emacs.d folder.")
 
+(defvar custom-emacs-dir (expand-file-name "custom" emacs-dir)
+  "The dir that contains the custom emacs file.")
+
+(setq custom-file (expand-file-name "custom-emacs.el" custom-emacs-dir))
+(load custom-file)
+
 (defvar config-emacs-dir (expand-file-name "config" emacs-dir)
   "Config files for all packages.")
 
 (when (file-exists-p config-emacs-dir)
   (message "Loading personal configuration files in %s..." config-emacs-dir)
     (mapc 'load (directory-files config-emacs-dir 't "^[^#].*el$")))
-
-(custom-set-variables
- '(inhibit-startup-screen t))
