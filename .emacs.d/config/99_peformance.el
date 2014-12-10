10d9878ee3165e7d8a637cee9140ca7114092a70
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2014-12-10 00:35:57 +0000

    * ensure that emacs gui reads $PATH

--- .emacs.d/config/99_peformance.el
+++ .emacs.d/config/99_peformance.el
@@ -1,1 +1,7 @@
 (setq gc-cons-threshold 50000000)
+(let ((path (shell-command-to-string ". ~/.zprofile; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path 
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
