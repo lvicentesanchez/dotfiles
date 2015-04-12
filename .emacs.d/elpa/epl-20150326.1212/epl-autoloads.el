2f808142a6955e0f014718167902fe8f3b92b6ee
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-04-12 23:58:02 +0100

    * update packages.

---
+++ .emacs.d/elpa/epl-20150326.1212/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl.el") (21802 63574 29 642000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
