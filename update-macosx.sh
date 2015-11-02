35fe872c1fa2fc7e4d9e12ac092455414319f7b8
Luis Sanchez <luis.sanchez@williamhill.com>
2015-11-02 10:32:09 +0000

    * update config files.

--- update-macosx.sh
+++ update-macosx.sh
@@ -3,9 +3,9 @@
 #
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/after/home/.zshrc.after ~/.zshrc.after
+ln -s ~/.dotfiles/after/ubuntu/.zshrc.after ~/.zshrc.after
 ln -s ~/.dotfiles/.zprofile ~/.zprofile
-ln -s ~/.dotfiles/after/home/.zprofile.after ~/.zprofile.after
+ln -s ~/.dotfiles/after/ubuntu/.zprofile.after ~/.zprofile.after
 ln -s ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
 
 ln -s ~/.dotfiles/.tmux ~/.tmux
---
+++ update-ubuntu.sh
@@ -0,0 +1,22 @@
+# This script installs/updates my configuration scripts
+#
+#
+
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/after/ubuntu/.zshrc.after ~/.zshrc.after
+ln -s ~/.dotfiles/.zprofile ~/.zprofile
+ln -s ~/.dotfiles/after/ubuntu/.zprofile.after ~/.zprofile.after
+ln -s ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
+
+ln -s ~/.dotfiles/.tmux ~/.tmux
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.tmux.binds.conf ~/.tmux.binds.conf
+
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim ~/.vim
+
+ln -s ~/.dotfiles/.fonts ~/.fonts
+ln -s ~/.dotfiles/.fonts.conf.d ~/.fonts.conf.d
+sudo fc-cache -vf ~/.fonts
+
+ln -s ~/.dotfiles/.dir_colors ~/.dir_colors
