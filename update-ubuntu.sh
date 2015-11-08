d1b146ec3d1d663ab47b61e02a33315dac5fb1f8
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-11-08 19:49:04 +0000

    * tweak scripts to install .dotfiles under ubuntu.

--- update-ubuntu.sh
+++ update-ubuntu.sh
@@ -2,18 +2,11 @@
 #
 #
 
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/after/ubuntu/.zshrc.after ~/.zshrc.after
-ln -s ~/.dotfiles/.zprofile ~/.zprofile
-ln -s ~/.dotfiles/after/ubuntu/.zprofile.after ~/.zprofile.after
-ln -s ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
-
-ln -s ~/.dotfiles/.tmux ~/.tmux
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/.tmux.binds.conf ~/.tmux.binds.conf
-
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+
+ln -s ~/.dotfiles/.vim ~/.config/nvim
+ln -s ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
 
 ln -s ~/.dotfiles/.fonts ~/.fonts
 ln -s ~/.dotfiles/.fonts.conf.d ~/.fonts.conf.d
